1eff613889319dcb6cdf1df0783a10a71e2e60dc
lukemunsch <lpmunsch@gmail.com>
2021-11-18 15:22:19 +0000

    completing arrow function section

--- arrow-functions.js
+++ arrow-functions.js
@@ -7,8 +7,34 @@
 
 // Arrow Function With Parameters
 
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Single Line Arrow Function With Parameters
+
+const addTwoNumbers2 = (a, b) => a + b;
+
+let sum2 = addTwoNumbers2(2, 5);
+console.log(sum2);
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+
+saySomething('Hi, Peoples');
+
+const sayHello = () => console.log("Hello");
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMunltipleLines = () => (
+    `<p>
+        This is a multiline string!
+    </p>`
+)
+console.log(returnMunltipleLines());
